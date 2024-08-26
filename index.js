16c93d0557394692c15ec56daf01b1417a2d30ca
nmmlnwza <jbq1551@gmail.com>
2024-08-26 09:12:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -81,27 +81,6 @@ var swiper = new Swiper(".books-slider", {
     },
   });
 
-  var swiper = new Swiper(".arrivals-slider", {
-    spaceBetween:10,
-    loop:true,
-    centeredSlides: true,
-    autoplay: {
-        delay: 1700,
-        disableOnInteraction: false,
-    },
-    breakpoints: {
-      0: {
-        slidesPerView: 1,
-      },
-      768: {
-        slidesPerView: 2,
-      },
-      1024: {
-        slidesPerView: 3,
-      },
-    },
-  });
-
   function search() {
     let filter = document.getElementById('find').value.toUpperCase();
     let item = document.querySelectorAll('.product');
